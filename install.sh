94f65f12ac91357e0f467e6d2b3f903d226e7aaa
jjalowie <103130376+jjalowie@users.noreply.github.com>
2022-11-30 11:08:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,11 +17,6 @@ wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai
 mkdir -p $INSTALL_PATH/.vim/colors
 mv -f monokai.vim $INSTALL_PATH/.vim/colors/monokai.vim
 
-if [ -e "~/.screenrc" ]; then
-  e=$(grep '^shell ' ~/.screenrc)
-  if [ e != "" ]; then
-    echo "shell -$SHELL" >> ~/.screenrc
-  fi
-else
-  echo "shell -$SHELL" >> ~/.screenrc
-fi >/dev/null
+wget -O .vimrc https://raw.githubusercontent.com/jjalowie/vim-config/main/.screenrc >/dev/null
+sed -i "s|INSTALL_PATH|$INSTALL_PATH|g" .screenrc
+mv -f .screenrc $INSTALL_PATH
