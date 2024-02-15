d5c61bc757f7fb7e18f4bb334a46e0d149b24475
jjalowie <jakub.jalowiec@intel.com>
2024-02-15 18:30:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ echo "Installing vim config to $INSTALL_PATH..."
 git clone https://github.com/gmarik/Vundle.vim.git $INSTALL_PATH/.vim/plugin/Vundle.vim
 curl -O https://raw.githubusercontent.com/jjalowie/vim-config/main/.vimrc
 sed -i "s|INSTALL_PATH|$INSTALL_PATH|g" .vimrc
-mv -f .vimrc $INSTALL_PATH
+mv -n .vimrc $INSTALL_PATH
 
 curl -O https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 mkdir -p $INSTALL_PATH/.vim/colors
-mv -f monokai.vim $INSTALL_PATH/.vim/colors/monokai.vim
+mv -n monokai.vim $INSTALL_PATH/.vim/colors/monokai.vim
